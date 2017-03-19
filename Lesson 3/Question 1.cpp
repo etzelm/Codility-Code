9aaf914d593770b617bd36db6d4c1bff2d606a25
Mitchell Etzel <metzel@ucsc.edu>
2017-03-19 13:06:34 -0700

    Update1

--- Lesson 3/Question 1.cpp
+++ Lesson 3/Question 1.cpp
@@ -2,14 +2,11 @@
 #include <iostream>
 
 int solution(int X, int Y, int D) {
-    long double x = X;
-    long double y = Y;
-    long double d = D;
+    long double x = X, y = Y, d = D;
     long double tmp = ceil((y-x)/d);
     return(tmp);
 }
 
 int main() {
     std::cout<< solution(10,85,30);
-
 }
